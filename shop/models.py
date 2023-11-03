9b05e33b9f2d796de40205884d91e667d9741cda
Antony-M1 <antony.praveenkumar@softsuave.com>
2023-11-03 16:31:15 +0530

    requiremets.txt

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'username'
+    
+    def __str__(self) -> str:
+        return self.email
+    
+    class Meta:
+        db_table = "tabUser"
+        verbose_name = 'User'
+        
+
+
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=500)
+    actual_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    offer_percentage = models.DecimalField(max_digits=3, decimal_places=2)
+    final_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self) -> str:
+        pass
+    
+    class Meta:
+        db_table = "tabProduct"
+        verbose_name = 'Product'
