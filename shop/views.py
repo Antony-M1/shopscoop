de0b75a05563decf0736f615c70b82991b0a4c2e
Antony-M1 <antony.praveenkumar@softsuave.com>
2023-11-04 11:06:00 +0530

    Product-Card-added

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 
 def home(request):
-    return render(request, template_name='shop/home.html', status=200)
+    product_details = Product.objects.all()
+    context = {
+        'product_data': product_details
+    }
+    return render(request, template_name='shop/home.html', status=200, context=context)
