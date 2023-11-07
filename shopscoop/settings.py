b9fb5e4c1e30da045d720439286c249bfa4725f7
Antony-M1 <antony.praveenkumar@softsuave.com>
2023-11-07 20:01:05 +0530

    Gunicorn and Cors

--- shopscoop/settings.py
+++ shopscoop/settings.py
@@ -43,12 +43,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop',
     'rest_framework',
-    'markdownx'
+    'markdownx',
+    
+    'corsheaders'
 ]
+
+CORS_ALLOWED_ORIGINS = ['*']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
