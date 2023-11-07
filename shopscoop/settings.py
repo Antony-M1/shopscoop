4bf50d6e7454a6668a3babf13f7a5db12d98ae86
Antony-M1 <antony.praveenkumar@softsuave.com>
2023-11-07 18:07:45 +0530

    env changes

--- shopscoop/settings.py
+++ shopscoop/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z@b*33s2qiayva^&)-js$ivdk95&a*13i4-u35c&2y(4bnw8dg'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG')))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
